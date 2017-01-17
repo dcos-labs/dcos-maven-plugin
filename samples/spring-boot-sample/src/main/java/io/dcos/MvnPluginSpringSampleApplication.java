2bfed44e6aca7f976c3c150bc175d901ec441d69
Johannes Unterstein <junterstein@mesosphere.io>
2017-01-17 11:57:43 +0100

    added spring boot sample

---
+++ samples/spring-boot-sample/src/main/java/io/dcos/MvnPluginSpringSampleApplication.java
@@ -0,0 +1,20 @@
+package io.dcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController("/")
+public class MvnPluginSpringSampleApplication {
+
+  @RequestMapping("/")
+  public String randomJoke() {
+    return "Hi!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(MvnPluginSpringSampleApplication.class, args);
+  }
+}
